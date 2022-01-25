13d69bb8fc45f2964c4190ce57062af79d1f8dfd
André Gonzalez <lopescg@gmail.com>
2022-01-25 12:13:36 -0300

    changed font size

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 /* this line below is to use when i am using only my laptop screeen the next one is fot TV*/
 /*static char *font = "Liberation Mono:pixelsize=30:antialias=true:autohint=true";*/
-static char *font = "Liberation Mono:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
