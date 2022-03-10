68556fb93c1f224a8cc0a746727ff14727ce40dc
André Gonzalez <lopescg@gmail.com>
2022-03-10 20:28:35 -0300

    changed font size for laptop screen

--- config.h
+++ config.h
@@ -6,8 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* this line below is to use when i am using only my laptop screeen the next one is fot TV*/
-/*static char *font = "Liberation Mono:pixelsize=30:antialias=true:autohint=true";*/
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=23:antialias=true:autohint=true";
+/* static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true"; */
 static int borderpx = 2;
 
 /*
